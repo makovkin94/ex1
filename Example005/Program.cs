ed08bb6b8a08365d57cd60f4b394374afe803175
ruslan <rmakovkin2@mail.com>
2022-12-02 00:27:07 +0300

    add example005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "руслан")
+{
+    Console.WriteLine("Здравствуйте, Хозяин");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
